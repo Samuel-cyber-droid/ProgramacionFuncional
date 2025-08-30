8927fd2a1aa5fa03b3899b520794f1da213503fd
Samuel-cyber-droid <samy21jul@gmail.com>
2025-08-30 14:41:36 -0600

    Se agregaron nuevos codigos (con functools)

--- main.py
+++ main.py
@@ -33,6 +33,31 @@ def fibonacci(n):
 def fibonacci2(n):
     return 0 if n == 0 else 1 if n == 1 else fibonacci(n - 1) + fibonacci(n - 2)
 
+#Serie de Fibonacci con Functools
+def fibonacci3(n) -> int:
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, n + 1):
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+        return fib_sequence[n]
+
+#Serie de Fibonacci mas eficiente
+def fibonacci4(n) -> int:
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+
 #multiplicacion a * b
 def mult2(a, b):
     return a * b
@@ -59,6 +84,16 @@ def potencia(n,p):
 def potencia2(n, p):
     return 1 if p == 0 else n * potencia(n, p - 1)
 
+#potencia de numero con functools
+def potencia3(n, p) -> float:
+    if p == 0:
+        return 1
+    elif p > 0:
+        return reduce(lambda x, y: x * n, range(p), 1)
+    else:
+        return 1 / reduce(lambda x, y: x * n, range(abs(p), 1))
+
+
 #Conteo de digitos
 def contar_digitos_iterativo(numero):
     # Convertir a string para manejar números negativos
@@ -80,21 +115,31 @@ def contar_digitos_funcional_completo(numero):
 # Suma de tupla (7, 9, 12, 21)
 
 # Ejecuciones
+print('Factoriales')
 print(factorial(5))
-print(factorial2(6))
-print(factorial3(7))
+print(factorial2(5))
+print(factorial3(5))
 
+print('Series de Fibonacci')
 print(fibonacci(10))
 print(fibonacci2(10))
+print(fibonacci3(10))
+print(fibonacci4(10))
 
+print('Multiplicaciones')
 print(resultado)
 print(resultado2) #con lambda
 
 print(mult2(2,6))
 
-print(potencia(4, 4))
-print(potencia2(5,8))
 
+print('Potencias')
+print(potencia(4, 4))
+print(potencia2(4,4))
+print(potencia3(4, 4))
+
+
+print('Conteo de Digitos')
 print(contar_digitos_iterativo(12345))
 print(contar_digitos_funcional_completo(145))
 
